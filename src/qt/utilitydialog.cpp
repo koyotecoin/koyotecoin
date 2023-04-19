a9899be4374072256142bc3b23e9b6b0239297ae
Eduardo Doege <eduardo.doege@outlook.com>
2023-04-19 10:26:37 -0300

    koyotecoin: add icons and fix ui form - qt/forms/helpmessagedialog.ui, qt/utilitydialog.cpp

--- src/qt/utilitydialog.cpp
+++ src/qt/utilitydialog.cpp
@@ -15,12 +15,13 @@
 
 #include <clientversion.h>
 #include <init.h>
-#include <util/system.h>
 #include <util/strencodings.h>
+#include <util/system.h>
 
 #include <stdio.h>
 
 #include <QCloseEvent>
+#include <QGroupBox>
 #include <QLabel>
 #include <QMainWindow>
 #include <QRegularExpression>
@@ -30,81 +31,84 @@
 #include <QVBoxLayout>
 
 /** "Help message" or "About" dialog box */
-HelpMessageDialog::HelpMessageDialog(QWidget *parent, bool about) :
-    QDialog(parent, GUIUtil::dialog_flags),
-    ui(new Ui::HelpMessageDialog)
+HelpMessageDialog::HelpMessageDialog(QWidget* parent, bool about) : QDialog(parent, GUIUtil::dialog_flags),
+                                                                    ui(new Ui::HelpMessageDialog)
 {
     ui->setupUi(this);
 
-    QString version = QString{PACKAGE_NAME} + " " + tr("version") + " " + QString::fromStdString(FormatFullVersion());
+    QString version = "<br>" + QString{PACKAGE_NAME} + " " + tr("version") + " " + QString::fromStdString(FormatFullVersion());
 
-    if (about)
-    {
-        setWindowTitle(tr("About %1").arg(PACKAGE_NAME));
+    // if (about) {
+    setWindowTitle(tr("About %1").arg(PACKAGE_NAME));
 
-        std::string licenseInfo = LicenseInfo();
-        /// HTML-format the license message from the core
-        QString licenseInfoHTML = QString::fromStdString(LicenseInfo());
-        // Make URLs clickable
-        QRegularExpression uri(QStringLiteral("<(.*)>"), QRegularExpression::InvertedGreedinessOption);
-        licenseInfoHTML.replace(uri, QStringLiteral("<a href=\"\\1\">\\1</a>"));
-        // Replace newlines with HTML breaks
-        licenseInfoHTML.replace("\n", "<br>");
+    std::string licenseInfo = LicenseInfo();
+    /// HTML-format the license message from the core
+    QString licenseInfoHTML = QString::fromStdString(LicenseInfo());
+    // Make URLs clickable
+    QRegularExpression uri(QStringLiteral("<(.*)>"), QRegularExpression::InvertedGreedinessOption);
+    licenseInfoHTML.replace(uri, QStringLiteral("<a href=\"\\1\">\\1</a>"));
+    // Replace newlines with HTML breaks
+    licenseInfoHTML.replace("\n", "<br>");
 
-        ui->aboutMessage->setTextFormat(Qt::RichText);
-        ui->scrollArea->setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
-        text = version + "\n" + QString::fromStdString(FormatParagraph(licenseInfo));
-        ui->aboutMessage->setText(version + "<br><br>" + licenseInfoHTML);
-        ui->aboutMessage->setWordWrap(true);
-        ui->helpMessage->setVisible(false);
-    } else {
-        setWindowTitle(tr("Command-line options"));
-        QString header = "Usage:  koyotecoin-qt [command-line options]                     \n";
-        QTextCursor cursor(ui->helpMessage->document());
-        cursor.insertText(version);
-        cursor.insertBlock();
-        cursor.insertText(header);
-        cursor.insertBlock();
+    // ui->aboutMessage->setTextFormat(Qt::RichText);
+    // ui->scrollArea->setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+    text = version + "\n" + QString::fromStdString(FormatParagraph(licenseInfo));
+    ui->aboutMessage->setText(version + "<br><br>" + licenseInfoHTML);
+    //
+    // Tests
+    // QGroupBox groupBox(ui->QDialog->document());
+    // groupBox.setFlat(true);
 
-        std::string strUsage = gArgs.GetHelpMessage();
-        QString coreOptions = QString::fromStdString(strUsage);
-        text = version + "\n\n" + header + "\n" + coreOptions;
+    // ui->aboutMessage->setWordWrap(true);
+    // ui->helpMessage->setVisible(false);
 
-        QTextTableFormat tf;
-        tf.setBorderStyle(QTextFrameFormat::BorderStyle_None);
-        tf.setCellPadding(2);
-        QVector<QTextLength> widths;
-        widths << QTextLength(QTextLength::PercentageLength, 35);
-        widths << QTextLength(QTextLength::PercentageLength, 65);
-        tf.setColumnWidthConstraints(widths);
+    // } else {
+    //     setWindowTitle(tr("Command-line options"));
+    //     QString header = "Usage:  koyotecoin-qt [command-line options]                     \n";
+    //     // QTextCursor cursor(ui->helpMessage->document());
+    //     cursor.insertText(version);
+    //     cursor.insertBlock();
+    //     cursor.insertText(header);
+    //     cursor.insertBlock();
 
-        QTextCharFormat bold;
-        bold.setFontWeight(QFont::Bold);
+    //     std::string strUsage = gArgs.GetHelpMessage();
+    //     QString coreOptions = QString::fromStdString(strUsage);
+    //     text = version + "\n\n" + header + "\n" + coreOptions;
 
-        for (const QString &line : coreOptions.split("\n")) {
-            if (line.startsWith("  -"))
-            {
-                cursor.currentTable()->appendRows(1);
-                cursor.movePosition(QTextCursor::PreviousCell);
-                cursor.movePosition(QTextCursor::NextRow);
-                cursor.insertText(line.trimmed());
-                cursor.movePosition(QTextCursor::NextCell);
-            } else if (line.startsWith("   ")) {
-                cursor.insertText(line.trimmed()+' ');
-            } else if (line.size() > 0) {
-                //Title of a group
-                if (cursor.currentTable())
-                    cursor.currentTable()->appendRows(1);
-                cursor.movePosition(QTextCursor::Down);
-                cursor.insertText(line.trimmed(), bold);
-                cursor.insertTable(1, 2, tf);
-            }
-        }
+    //     QTextTableFormat tf;
+    //     tf.setBorderStyle(QTextFrameFormat::BorderStyle_None);
+    //     tf.setCellPadding(2);
+    //     QVector<QTextLength> widths;
+    //     widths << QTextLength(QTextLength::PercentageLength, 35);
+    //     widths << QTextLength(QTextLength::PercentageLength, 65);
+    //     tf.setColumnWidthConstraints(widths);
 
-        ui->helpMessage->moveCursor(QTextCursor::Start);
-        ui->scrollArea->setVisible(false);
-        ui->aboutLogo->setVisible(false);
-    }
+    //     QTextCharFormat bold;
+    //     bold.setFontWeight(QFont::Bold);
+
+    //     for (const QString& line : coreOptions.split("\n")) {
+    //         if (line.startsWith("  -")) {
+    //             cursor.currentTable()->appendRows(1);
+    //             cursor.movePosition(QTextCursor::PreviousCell);
+    //             cursor.movePosition(QTextCursor::NextRow);
+    //             cursor.insertText(line.trimmed());
+    //             cursor.movePosition(QTextCursor::NextCell);
+    //         } else if (line.startsWith("   ")) {
+    //             cursor.insertText(line.trimmed() + ' ');
+    //         } else if (line.size() > 0) {
+    //             // Title of a group
+    //             if (cursor.currentTable())
+    //                 cursor.currentTable()->appendRows(1);
+    //             cursor.movePosition(QTextCursor::Down);
+    //             cursor.insertText(line.trimmed(), bold);
+    //             cursor.insertTable(1, 2, tf);
+    //         }
+    //     }
+
+    //     // ui->helpMessage->moveCursor(QTextCursor::Start);
+    //     // ui->scrollArea->setVisible(false);
+    //     ui->aboutLogo->setVisible(false);
+    // }
 
     GUIUtil::handleCloseWindowShortcut(this);
 }
@@ -138,10 +142,9 @@ void HelpMessageDialog::on_okButton_accepted()
 
 
 /** "Shutdown" window */
-ShutdownWindow::ShutdownWindow(QWidget *parent, Qt::WindowFlags f):
-    QWidget(parent, f)
+ShutdownWindow::ShutdownWindow(QWidget* parent, Qt::WindowFlags f) : QWidget(parent, f)
 {
-    QVBoxLayout *layout = new QVBoxLayout();
+    QVBoxLayout* layout = new QVBoxLayout();
     layout->addWidget(new QLabel(
         tr("%1 is shutting down…").arg(PACKAGE_NAME) + "<br /><br />" +
         tr("Do not shut down the computer until this window disappears.")));
@@ -155,7 +158,7 @@ QWidget* ShutdownWindow::showShutdownWindow(QMainWindow* window)
     assert(window != nullptr);
 
     // Show a simple window indicating shutdown status
-    QWidget *shutdownWindow = new ShutdownWindow();
+    QWidget* shutdownWindow = new ShutdownWindow();
     shutdownWindow->setWindowTitle(window->windowTitle());
 
     // Center shutdown window at where main window was
@@ -165,7 +168,7 @@ QWidget* ShutdownWindow::showShutdownWindow(QMainWindow* window)
     return shutdownWindow;
 }
 
-void ShutdownWindow::closeEvent(QCloseEvent *event)
+void ShutdownWindow::closeEvent(QCloseEvent* event)
 {
     event->ignore();
 }
