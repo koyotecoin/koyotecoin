0e98cb257e89c3696b3a46270c0e6de85dcf62e0
Eduardo Doege <genisyskernel@gmail.com>
2023-06-27 21:14:58 -0300

    src/qt: update icon position on splashscreen.cpp

--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -67,7 +67,7 @@ SplashScreen::SplashScreen(const NetworkStyle* networkStyle)
     pixPaint.fillRect(rGradient, gradient);
 
     // draw the koyotecoin icon, expected size of PNG: 1024x1024
-    QRect rectIcon(QPoint(-50, -45), QSize(300, 300));
+    QRect rectIcon(QPoint(-65, -45), QSize(300, 300));
 
     const QSize requiredSize(1024, 1024);
     QPixmap icon(networkStyle->getAppIcon().pixmap(requiredSize));
