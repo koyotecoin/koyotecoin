12f8d88d6d2fdea2a9a59b07b6013a67a373a3ae
Eduardo Doege <genisyskernel@gmail.com>
2023-09-02 21:37:51 -0300

    src: update version

--- src/version.h
+++ src/version.h
@@ -10,34 +10,34 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 100;
+static const int PROTOCOL_VERSION = 10000;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 100;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 100;
+static const int MIN_PEER_PROTO_VERSION = 10000;
 
 //! BIP 0031, pong message, is enabled for all versions AFTER this one
-static const int BIP0031_VERSION = 100;
+static const int BIP0031_VERSION = 10000;
 
 //! "filter*" commands are disabled without NODE_BLOOM after and including this version
-static const int NO_BLOOM_VERSION = 100;
+static const int NO_BLOOM_VERSION = 10000;
 
 //! "sendheaders" command and announcing blocks with headers starts with this version
-static const int SENDHEADERS_VERSION = 100;
+static const int SENDHEADERS_VERSION = 10000;
 
 //! "feefilter" tells peers to filter invs to you by fee starts with this version
-static const int FEEFILTER_VERSION = 100;
+static const int FEEFILTER_VERSION = 10000;
 
 //! short-id-based block download starts with this version
-static const int SHORT_IDS_BLOCKS_VERSION = 100;
+static const int SHORT_IDS_BLOCKS_VERSION = 10000;
 
 //! not banning for invalid compact blocks starts with this version
-static const int INVALID_CB_NO_BAN_VERSION = 100;
+static const int INVALID_CB_NO_BAN_VERSION = 10000;
 
 //! "wtxidrelay" command for wtxid-based relay starts with this version
-static const int WTXID_RELAY_VERSION = 100;
+static const int WTXID_RELAY_VERSION = 10000;
 
 // Make sure that none of the values above collide with
 // `SERIALIZE_TRANSACTION_NO_WITNESS` or `ADDRV2_FORMAT`.
